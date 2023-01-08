c602d42aa239cb3735fa1afbc233082bdf34ff3e
Gjklmk <69299755+Gjklmk@users.noreply.github.com>
2023-01-08 19:19:14 +0800

    Update geek.py

--- geek.py
+++ geek.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 import os
@@ -11,7 +12,7 @@ r = requests.get(url)
 
 # 解析
 soup = BeautifulSoup(r.text, 'html.parser')
-texts = soup.find_all('div', class_="modal-text")
+texts = soup.get_text()
 
 photo_urls = soup.find_all('img')
 
@@ -19,15 +20,32 @@ photo_urls = soup.find_all('img')
 # 保存
 # 文本
 textf = open("geek.text", "w", encoding='utf8')
-for text in texts:
-    textf.write(text.text)
+# for text in texts:
+textf.write(texts)
 
 textf.close()
 
 
 # 图片
-#os.makedirs('./img/', exist_ok=True)
-# for photo_url in photo_urls:
+os.makedirs('./img/', exist_ok=True)
+for photo_url in photo_urls:
+    img = "https://itawenya.cn" + photo_url['src']
+    q = requests.get(img, stream=True)
+    image_name = img.split('/')[-1]
+    with open('./img/%s' % image_name, 'wb') as f:
+        f.write(q.content)
+        f.flush()
+
+
+# requests.adapters.DEFAULT_RETRIES = 5
+# s = requests.session()
+# s.keep_alive = False
+# q = s.get(img)
+# image_name = img.split('/')[-1]
+# with open('./img/%s' % image_name, 'wb') as f:
+#     f.write(q.content)
+
+
 #photo = open("geek.photo", "wb")
 # photo.write(photo_urls)
 
@@ -36,8 +54,9 @@ textf.close()
 
 # for photo_url in photo_urls:
 #     img = photo_url['src']
-#     q = requests.get(img)
+#     q = requests.get(img, stream=True)
 #     image_name = img.split('/')[-1]
 #     with open('./img/%s' % image_name, 'wb') as f:
 #         for chunk in q.iter_content(chunk_size=128):
 #             f.write(chunk)
+#             f.flush()
